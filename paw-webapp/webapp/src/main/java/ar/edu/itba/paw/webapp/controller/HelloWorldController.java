4bbe02d39843e3fa4317e6c0b6108f95e9efad74
ssandrini2 <ssandrini@itba.edu.ar>
2022-04-03 15:41:35 -0300

    auditionService in helloworldController

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -1,7 +1,9 @@
 package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.exceptions.UserNotFoundException;
+import ar.edu.itba.paw.model.Audition;
 import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.service.AuditionService;
 import ar.edu.itba.paw.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,22 +11,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class HelloWorldController {
 
 
     private final UserService us;
+    private final AuditionService as;
 
     @Autowired
-    public HelloWorldController(UserService us) {
+    public HelloWorldController(UserService us, AuditionService as) {
         this.us = us;
+        this.as = as;
     }
 
     @RequestMapping("/helloWorld")
     public ModelAndView helloWorld(@RequestParam(name = "userId", defaultValue = "1") final long userId) {
 
         final ModelAndView mav = new ModelAndView("index");
-
+        List<Audition> auditionList = as.getAll(1);
+        mav.addObject("auditionList", auditionList);
         mav.addObject("user", us.getUserById(userId).orElseThrow(UserNotFoundException::new));
         return mav;
     }
