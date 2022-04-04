3368a609d90637cc470ca3014e98fe9ab1bee458
Federico Rojas <frojas@itba.edu.ar>
2022-04-04 16:21:22 -0300

    Pom test dependencies added

--- paw-webapp/persistence/src/test/java/ar/edu/itba/paw/persistence/TestConfig.java
+++ paw-webapp/persistence/src/test/java/ar/edu/itba/paw/persistence/TestConfig.java
@@ -1,2 +1,18 @@
-package ar.edu.itba.paw.persistence;public class TestConfig {
+package ar.edu.itba.paw.persistence;
+
+import org.hsqldb.jdbc.JDBCDriver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+
+import javax.sql.DataSource;
+
+public class TestConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        final SimpleDriverDataSource ds = new SimpleDriverDataSource();
+        ds.setDriverClass(JDBCDriver.class);
+        ds.setUrl("jdbc:");
+    }
 }
