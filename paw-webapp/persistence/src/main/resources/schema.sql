34f3708bfbca0c8ed014c5f5f440271e4d5e00c5
Federico Rojas <frojas@itba.edu.ar>
2022-04-08 10:30:25 -0300

    SQL tables redesign

--- paw-webapp/persistence/src/main/resources/schema.sql
+++ paw-webapp/persistence/src/main/resources/schema.sql
@@ -1,26 +1,48 @@
+CREATE TABLE IF NOT EXISTS locations
+(
+    id SERIAL PRIMARY KEY,
+    location VARCHAR(100) UNIQUE NOT NULL
+)
+
 CREATE TABLE IF NOT EXISTS auditions
 (
     id SERIAL PRIMARY KEY,
     bandId INT NOT NULL,
     title VARCHAR(100) UNIQUE NOT NULL,
     description VARCHAR(512) NOT NULL,
-    creationDate DATE NOT NULL,
-    location VARCHAR(100) NOT NULL
+    creationDate TIMESTAMP NOT NULL,
+    locationId integer NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    FOREIGN KEY (locationId) REFERENCES locations ON DELETE SET NULL
 );
 -- TODO : FOREIGN KEY(band_id) REFERENCES Band(band_id) ON DELETE CASCADE
 
+CREATE TABLE IF NOT EXISTS genres
+(
+    id SERIAL PRIMARY KEY,
+    genre VARCHAR(100) UNIQUE NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS roles
+(
+    id SERIAL PRIMARY KEY,
+    role VARCHAR(100) UNIQUE NOT NULL
+)
+
 CREATE TABLE IF NOT EXISTS auditionGenres
 (
-    id INT NOT NULL,
-    genre VARCHAR(50) NOT NULL,
-    PRIMARY KEY(id, genre),
-    FOREIGN KEY(id) REFERENCES auditions(id) ON DELETE CASCADE
+    auditionId INTEGER NOT NULL,
+    genreId INTEGER NOT NULL,
+    FOREIGN KEY(auditionId) references auditions(id) ON DELETE CASCADE,
+    FOREIGN KEY(genreId) references genres(id) ON DELETE SET NULL,
+    UNIQUE(auditionId, genreId)
 );
 
 CREATE TABLE IF NOT EXISTS auditionRoles
 (
-    id INT NOT NULL,
-    role VARCHAR(50) NOT NULL,
-    PRIMARY KEY(id, role),
-    FOREIGN KEY(id) REFERENCES auditions(id) ON DELETE CASCADE
+    auditionId INTEGER NOT NULL,
+    roleId INTEGER NOT NULL,
+    FOREIGN KEY(auditionId) REFERENCES auditions(id) ON DELETE CASCADE,
+    FOREIGN KEY(roleId) references roles(id) ON DELETE SET NULL,
+    UNIQUE(auditionId, roleId)
 );
