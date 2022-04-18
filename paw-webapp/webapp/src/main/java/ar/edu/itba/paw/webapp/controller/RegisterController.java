8bec93719468567057bc4b0ab75b41dce1c2a20f
ssandrini2 <ssandrini@itba.edu.ar>
2022-04-17 21:16:13 -0300

    Register added

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/RegisterController.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/RegisterController.java
@@ -2,11 +2,17 @@ package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.model.User;
 import ar.edu.itba.paw.service.UserService;
+import ar.edu.itba.paw.webapp.form.ApplicationForm;
+import ar.edu.itba.paw.webapp.form.UserForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class RegisterController {
@@ -19,14 +25,25 @@ public class RegisterController {
     }
 
     @RequestMapping(value = "/register", method = {RequestMethod.POST})
-    public ModelAndView register() {
-        User.UserBuilder user = new User.UserBuilder("santi@mail.com","hola","santi",false,false);
+    public ModelAndView register(@Valid @ModelAttribute("userForm") final UserForm userForm,
+                                 final BindingResult errors) {
+
+        if (errors.hasErrors()) {
+            return registerView(userForm);
+        }
+
+        User.UserBuilder user = new User.UserBuilder(
+                userForm.getEmail(), userForm.getPassword(),
+                userForm.getName(), userForm.isBand(),
+                userForm.isAdmin()
+        ).surname(userForm.getSurname());
+
         userService.create(user);
-        return new ModelAndView("views/auditions");
+        return new ModelAndView("views/welcome");
     }
 
     @RequestMapping(value = "/register", method = {RequestMethod.GET})
-    public ModelAndView registerView() {
+    public ModelAndView registerView( @ModelAttribute("userForm") final UserForm userForm) {
         return new ModelAndView("views/register");
     }
 
---
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/form/UserForm.java
@@ -0,0 +1,75 @@
+package ar.edu.itba.paw.webapp.form;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.Size;
+
+public class UserForm {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 8, max = 25)
+    private String password;
+
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(max = 50)
+    private String surname;
+
+    private boolean isBand, isAdmin;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public boolean isBand() {
+        return isBand;
+    }
+
+    public void setBand(boolean band) {
+        isBand = band;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
