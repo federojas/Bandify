e6da82637c9b40d8d61e796189b70e3c4f775fef
Federico Rojas <frojas@itba.edu.ar>
2022-05-01 17:30:42 -0300

    New max file size annotation added

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/form/UserEditForm.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/form/UserEditForm.java
@@ -1,6 +1,8 @@
 package ar.edu.itba.paw.webapp.form;
 
+import ar.edu.itba.paw.webapp.form.constraints.annotations.MaxFileSize;
 import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.validation.constraints.Size;
 
@@ -17,6 +19,17 @@ public class UserEditForm {
     @NotBlank
     @Size(max = 500)
     private String description;
+
+    @MaxFileSize(8)
+    private CommonsMultipartFile profileImage;
+
+    public CommonsMultipartFile getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(CommonsMultipartFile profileImage) {
+        this.profileImage = profileImage;
+    }
 
     public String getName() {
         return name;
