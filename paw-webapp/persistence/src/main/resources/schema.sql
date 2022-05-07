666f4aa2e75add1a0636f8fd233c04407a6cfaf9
Santiago Sandrini <ssandrini@itba.edu.ar>
2022-05-07 14:17:56 -0300

    applications added to schema.sql

--- paw-webapp/persistence/src/main/resources/schema.sql
+++ paw-webapp/persistence/src/main/resources/schema.sql
@@ -91,3 +91,13 @@ CREATE TABLE IF NOT EXISTS profileImages
    image    BYTEA,
    FOREIGN KEY (userId) REFERENCES users ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS applications
+(
+    auditionId INTEGER NOT NULL,
+    applicantId INTEGER NOT NULL,
+    state TEXT NOT NULL,
+    PRIMARY KEY(auditionId,applicantId),
+    FOREIGN KEY (auditionId) REFERENCES auditions(id) ON DELETE CASCADE,
+    FOREIGN KEY (applicantId) REFERENCES users(id) ON DELETE CASCADE
+);
