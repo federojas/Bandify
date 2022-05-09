e14162aba01bef019af343e29827336fbe2b1351
robrodriguez99 <robrodriguez@itba.edu.ar>
2022-05-09 19:40:15 -0300

    js check for edit artist profile

--- paw-webapp/webapp/src/main/webapp/resources/js/editProfile.js
+++ paw-webapp/webapp/src/main/webapp/resources/js/editProfile.js
@@ -4,22 +4,30 @@ function editArtistFormCheck() {
     let artistSurname = document.getElementById("artistSurname");
     let artistDescription = document.getElementById("artistDescription");
     form.addEventListener('submit', function (e) {
-        if(artistName.value.length<0||artistName.value.length>50 ){
+
+        let wrongArtistName=document.getElementById("wrongArtistName");
+        let wrongArtistDescription=document.getElementById("wrongArtistDescription");
+        let wrongArtistSurname=document.getElementById("wrongArtistSurname");
+        wrongArtistName.style.display='none'
+        wrongArtistSurname.style.display='none'
+        wrongArtistDescription.style.display='none'
+
+        if(artistName.value.length<=0||artistName.value.length>50 ){
             e.preventDefault();
-            // snackbarMessage()
-            alert("hola");
+            wrongArtistName.style.display='block'
+            snackbarMessage()
 
         }
-        if(artistSurname.value.length<0||artistSurname.value.length>50 ){
+        if(artistSurname.value.length<=0||artistSurname.value.length>50 ){
             e.preventDefault();
-            // snackbarMessage()
-            alert("hola");
+            wrongArtistSurname.style.display='block'
+            snackbarMessage()
 
         }
-        if(artistDescription.value.length<0||artistDescription.value.length>500 ){
+        if(artistDescription.value.length<=0||artistDescription.value.length>500 ){
             e.preventDefault();
-            // snackbarMessage()
-            alert("hola");
+            wrongArtistDescription.style.display='block'
+            snackbarMessage()
 
         }
     });
