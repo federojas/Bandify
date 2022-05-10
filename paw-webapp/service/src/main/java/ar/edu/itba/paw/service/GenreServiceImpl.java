1fcf6ddc3ea49463cfecfb89f0b81cbfdb97f38f
Federico Rojas <frojas@itba.edu.ar>
2022-05-10 14:26:18 -0300

    imports removed

--- paw-webapp/service/src/main/java/ar/edu/itba/paw/service/GenreServiceImpl.java
+++ paw-webapp/service/src/main/java/ar/edu/itba/paw/service/GenreServiceImpl.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
