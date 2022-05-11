8d9f19d427b123bb4290eb8193d5aef11dc6c2f8
Santiago Sandrini <ssandrini@itba.edu.ar>
2022-05-11 01:19:51 -0300

    appServiceTest fix

--- paw-webapp/service/src/test/java/ar/edu/itba/paw/service/ApplicationServiceTest.java
+++ paw-webapp/service/src/test/java/ar/edu/itba/paw/service/ApplicationServiceTest.java
@@ -36,13 +36,14 @@ public class ApplicationServiceTest {
     @InjectMocks
     private ApplicationService applicationService = new ApplicationServiceImpl();
 
-    private final Application pendingApp = new Application.ApplicationBuilder(1,1,ApplicationState.PENDING, LocalDateTime.MIN).build();
-    private final Application rejectedApp = new Application.ApplicationBuilder(1,1,ApplicationState.REJECTED, LocalDateTime.MIN).build();
-    private final Application acceptedApp = new Application.ApplicationBuilder(1,1,ApplicationState.ACCEPTED, LocalDateTime.MIN).build();
-    private final User userWhoAlreadyApplied = new User.UserBuilder("email@email.com","password","name",false,true).id(1).build();
-    private final User userWhoDidntApplied = new User.UserBuilder("email2@email.com","password","name",false,true).id(3).build();
-    private final User band = new User.UserBuilder("band@email.com","password","band",true,true).id(2).build();
-    private static final Audition aud1 = new Audition.AuditionBuilder("TITLE", "DESCRIPTION", 2, LocalDateTime.MIN).id(1).build();
+    private final Application PENDING_APP = new Application.ApplicationBuilder(1,1,ApplicationState.PENDING, LocalDateTime.MIN).build();
+    private final Application REJECTED_APP = new Application.ApplicationBuilder(1,1,ApplicationState.REJECTED, LocalDateTime.MIN).build();
+    private final Application ACCEPTED_APP = new Application.ApplicationBuilder(1,1,ApplicationState.ACCEPTED, LocalDateTime.MIN).build();
+    private final User USER_ALREADY_APPLIED = new User.UserBuilder("email@email.com","password","name",false,true).id(1).build();
+    private final User USER_NOT_APPLIED = new User.UserBuilder("email2@email.com","password","name",false,true).id(3).build();
+    private final User BAND = new User.UserBuilder("band@email.com","password","band",true,true).id(2).build();
+    private static final Audition AUDITION = new Audition.AuditionBuilder("TITLE", "DESCRIPTION", 2, LocalDateTime.MIN).id(1).build();
+
     @Test(expected = IllegalArgumentException.class)
     public void testGetAuditionApplicationsByStateWithIllegalPage() {
         applicationService.getAuditionApplicationsByState(1, ApplicationState.ALL,-1);
@@ -58,51 +59,51 @@ public class ApplicationServiceTest {
     @Test
     public void testGetAuditionApplicationsByStateWithPendingState() {
         when(applicationDao.getAuditionApplicationsByState(1,ApplicationState.PENDING,1)).
-                thenReturn(new ArrayList<>(Collections.singletonList(pendingApp)));
+                thenReturn(new ArrayList<>(Collections.singletonList(PENDING_APP)));
         List<Application> list = applicationService.getAuditionApplicationsByState(1,ApplicationState.PENDING,1);
         Assert.assertEquals(1,list.size());
-        Assert.assertTrue(list.containsAll(new ArrayList<>(Collections.singletonList(pendingApp))));
+        Assert.assertTrue(list.containsAll(new ArrayList<>(Collections.singletonList(PENDING_APP))));
     }
 
     @Test
     public void testGetAuditionApplicationsByStateWithAllState() {
         when(applicationDao.getAuditionApplicationsByState(1,ApplicationState.PENDING,1)).
-                thenReturn(new ArrayList<>(Collections.singletonList(pendingApp)));
+                thenReturn(new ArrayList<>(Collections.singletonList(PENDING_APP)));
         List<Application> list = applicationService.getAuditionApplicationsByState(1,ApplicationState.ALL,1);
         Assert.assertEquals(1,list.size());
-        Assert.assertTrue(list.containsAll(new ArrayList<>(Collections.singletonList(pendingApp))));
+        Assert.assertTrue(list.containsAll(new ArrayList<>(Collections.singletonList(PENDING_APP))));
     }
 
     @Test
     public void testGetAuditionApplicationsByStateWithAcceptedState() {
         when(applicationDao.getAuditionApplicationsByState(1,ApplicationState.ACCEPTED,1)).
-                thenReturn(new ArrayList<>(Collections.singletonList(acceptedApp)));
+                thenReturn(new ArrayList<>(Collections.singletonList(ACCEPTED_APP)));
         List<Application> list = applicationService.getAuditionApplicationsByState(1,ApplicationState.ACCEPTED,1);
         Assert.assertEquals(1,list.size());
-        Assert.assertTrue(list.containsAll(new ArrayList<>(Collections.singletonList(acceptedApp))));
+        Assert.assertTrue(list.containsAll(new ArrayList<>(Collections.singletonList(ACCEPTED_APP))));
     }
 
     @Test
     public void testGetAuditionApplicationsByStateWithRejectedState() {
         when(applicationDao.getAuditionApplicationsByState(1,ApplicationState.REJECTED,1)).
-                thenReturn(new ArrayList<>(Collections.singletonList(rejectedApp)));
+                thenReturn(new ArrayList<>(Collections.singletonList(REJECTED_APP)));
         List<Application> list = applicationService.getAuditionApplicationsByState(1,ApplicationState.REJECTED,1);
         Assert.assertEquals(1,list.size());
-        Assert.assertTrue(list.containsAll(new ArrayList<>(Collections.singletonList(rejectedApp))));
+        Assert.assertTrue(list.containsAll(new ArrayList<>(Collections.singletonList(REJECTED_APP))));
     }
 
     @Test
     public void testApplyWithUserWhoAlreadyAppliedThisAudition() {
-        when(applicationDao.exists(1,userWhoAlreadyApplied.getId())).thenReturn(true);
-        boolean applied = applicationService.apply(1,userWhoAlreadyApplied,"message");
+        when(applicationDao.exists(1, USER_ALREADY_APPLIED.getId())).thenReturn(true);
+        boolean applied = applicationService.apply(1, USER_ALREADY_APPLIED,"message");
         Assert.assertFalse(applied);
     }
 
     @Test
     public void testApplyWithUserWhoDidntAppliedThisAudition() {
-        when(auditionService.getAuditionById(1)).thenReturn(Optional.of(aud1));
-        when(userService.getUserById(2)).thenReturn(Optional.of(band));
-        boolean applied = applicationService.apply(1,userWhoDidntApplied,"message");
+        when(auditionService.getAuditionById(1)).thenReturn(Optional.of(AUDITION));
+        when(userService.getUserById(2)).thenReturn(Optional.of(BAND));
+        boolean applied = applicationService.apply(1, USER_NOT_APPLIED,"message");
         Assert.assertTrue(applied);
     }
 
