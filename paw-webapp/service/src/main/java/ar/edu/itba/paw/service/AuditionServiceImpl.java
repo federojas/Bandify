1527a5d45989843c67e892eec815cf4fe1636015
Federico Rojas <frojas@itba.edu.ar>
2022-05-11 01:25:33 -0300

    Audition service test added

--- paw-webapp/service/src/main/java/ar/edu/itba/paw/service/AuditionServiceImpl.java
+++ paw-webapp/service/src/main/java/ar/edu/itba/paw/service/AuditionServiceImpl.java
@@ -18,16 +18,13 @@ import java.util.*;
 @Service
 public class AuditionServiceImpl implements AuditionService {
 
-    private final AuditionDao auditionDao;
-    private final UserService userService;
+    @Autowired
+    private AuditionDao auditionDao;
+    @Autowired
+    private UserService userService;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(AuditionServiceImpl.class);
 
-    @Autowired
-    public AuditionServiceImpl(final AuditionDao auditionDao,
-                               final UserService userService) {
-        this.auditionDao = auditionDao;
-        this.userService = userService;
-    }
 
     @Override
     public Optional<Audition> getAuditionById(long id) {
