9d36db2c64515c63bebf3c5e28df81d10849b371
robrodriguez99 <robrodriguez@itba.edu.ar>
2022-05-25 11:47:17 -0300

    Role model JPA

--- paw-webapp/model/src/main/java/ar/edu/itba/paw/Role.java
+++ paw-webapp/model/src/main/java/ar/edu/itba/paw/Role.java
@@ -1,22 +1,59 @@
 package ar.edu.itba.paw;
 
+import javax.persistence.*;
 import java.util.Objects;
 
+@Entity
+@Table(name="roles")
 public class Role {
-    private final long id;
-    private final String name;
 
-    public Role(long id, String name) {
-        this.id = id;
-        this.name = name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "roles_roleid_seq")
+    @SequenceGenerator(sequenceName = "roles_roleid_seq",name="roles_roleid_seq")
+    private long roleId;
+
+    @Column
+    private String roleName;
+
+    /* package */ Role() {
+        // Just for Hibernate, we love you!
+    }
+    public Role(Builder builder) {
+        this.roleId = builder.roleId;
+        this.roleName = builder.roleName;
+    }
+
+
+    public static class Builder{
+        private String roleName;
+        private long roleId;
+
+        public Builder(){
+            //default
+        }
+
+        public Builder withRoleId(int roleId){
+            this.roleId=roleId;
+            return this;
+        }
+        public Builder withRoleName(String roleName){
+            this.roleName=roleName;
+            return this;
+        }
+        public Role build() {
+            if (this.roleName == null) {
+                throw new NullPointerException("The property \"roleName\" cannot be null. ");
+            }
+            return new Role(this);
+        }
     }
 
     public long getId() {
-        return id;
+        return roleId;
     }
 
     public String getName() {
-        return name;
+        return roleName;
     }
 
     @Override
@@ -29,6 +66,6 @@ public class Role {
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(roleId, roleName);
     }
 }
