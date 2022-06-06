c6c99278e48631a879ed2129284b40e20d9bdc81
Santiago Sandrini <ssandrini@itba.edu.ar>
2022-06-06 08:33:04 -0300

    VerificationTokenDaoTest fixed

--- paw-webapp/persistence/src/test/java/ar/edu/itba/paw/persistence/VerificationTokenDaoTest.java
+++ paw-webapp/persistence/src/test/java/ar/edu/itba/paw/persistence/VerificationTokenDaoTest.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.paw.persistence;
 
 import ar.edu.itba.paw.model.TokenType;
+import ar.edu.itba.paw.model.User;
 import ar.edu.itba.paw.model.VerificationToken;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,7 +14,8 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.transaction.annotation.Transactional;
-
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -23,8 +25,8 @@ import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 @Sql("classpath:verificationTokenDaoTest.sql")
-@Rollback
 @Transactional
+@Rollback
 public class VerificationTokenDaoTest {
 
     @Autowired
@@ -32,18 +34,17 @@ public class VerificationTokenDaoTest {
     @Autowired
     private DataSource ds;
 
+    @PersistenceContext
+    private EntityManager em;
+
     private JdbcTemplate jdbcTemplate;
 
     private static final long USER_ID = 1;
-    private static final long TOKEN_ID = 1;
-    private static final long OTHER_USER_ID = 2;
+    private static final User USER = new User.UserBuilder("email@email.com","secret","name",true,false).id(USER_ID).build();
     private static final long INVALID_ID = 20;
-
-    private static final String TOKEN = "token";
+    private static final String TOKEN = "c73bf3f5-8d3e-4ddd-8105-9acd870dc0ab";
     private static final String INVALID_TOKEN = "INVALIDO";
-
     private static final LocalDateTime EXPIRY_DATE = LocalDateTime.now().plusDays(1);
-
     private static final LocalDateTime USER_VERIFY_TOKEN_EXPIRY_DATE = LocalDateTime.of(2022 ,7, 5, 14, 23, 30);
     private static final String USER_VERIFY_TOKEN = "token-verify";
 
@@ -52,44 +53,45 @@ public class VerificationTokenDaoTest {
         jdbcTemplate = new JdbcTemplate(ds);
     }
 
-   // @Test
-//    public void testCreateToken() {
-//        VerificationToken token1 = verificationTokenDao.createToken(OTHER_USER_ID, TOKEN, EXPIRY_DATE, TokenType.VERIFY);
-//        assertNotNull(token1);
-//        assertEquals(OTHER_USER_ID, token1.getUserId());
-//        assertEquals(TOKEN, token1.getToken());
-//        assertEquals(EXPIRY_DATE, token1.getExpiryDate());
-//        assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "verificationtokens", "tokenid = " + token1.getId()));
-//    }
-//
-//    @Test
-//    public void testDeleteTokenByUserId() {
-//        verificationTokenDao.deleteTokenByUserId(USER_ID, TokenType.RESET);
-//        assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "verificationtokens", "userid = " + USER_ID));
-//    }
-//
-//    @Test
-//    public void testDeleteTokenByInvalidUserId() {
-//        verificationTokenDao.deleteTokenByUserId(INVALID_ID, TokenType.RESET);
-//        assertEquals(3, JdbcTestUtils.countRowsInTable(jdbcTemplate, "verificationtokens"));
-//    }
-//
-//    @Test
-//    public void testDeleteTokenByUserIdInvalidType() {
-//        verificationTokenDao.deleteTokenByUserId(INVALID_ID, TokenType.VERIFY);
-//        assertEquals(3, JdbcTestUtils.countRowsInTable(jdbcTemplate, "verificationtokens"));
-//    }
+    @Test
+    public void testCreateToken() {
+        VerificationToken token1 = verificationTokenDao.createToken(USER, TOKEN, EXPIRY_DATE, TokenType.VERIFY);
+        assertNotNull(token1);
+        assertEquals(USER, token1.getUser());
+        assertEquals(TOKEN, token1.getToken());
+        assertEquals(EXPIRY_DATE, token1.getExpiryDate());
+        assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "verificationtokens", "tokenid = " + token1.getId()));
+    }
 
-//    @Test
-//    public void testGetToken() {
-//        Optional<VerificationToken> verificationToken = verificationTokenDao.getToken(USER_VERIFY_TOKEN);
-//        assertNotNull(verificationToken);
-//        assertTrue(verificationToken.isPresent());
-//        assertEquals(2, verificationToken.get().getId());
-//        assertEquals(USER_VERIFY_TOKEN, verificationToken.get().getToken());
-//        assertEquals(TokenType.VERIFY, verificationToken.get().getType());
-//        assertEquals(verificationToken.get().getExpiryDate(), USER_VERIFY_TOKEN_EXPIRY_DATE);
-//    }
+    @Test
+    public void testDeleteTokenByUserId() {
+        verificationTokenDao.deleteTokenByUserId(USER_ID, TokenType.RESET);
+        em.flush();
+        assertEquals(1, JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "verificationtokens", "userid = " + USER_ID));
+    }
+
+    @Test
+    public void testDeleteTokenByInvalidUserId() {
+        verificationTokenDao.deleteTokenByUserId(INVALID_ID, TokenType.RESET);
+        assertEquals(3, JdbcTestUtils.countRowsInTable(jdbcTemplate, "verificationtokens"));
+    }
+
+    @Test
+    public void testDeleteTokenByUserIdInvalidType() {
+        verificationTokenDao.deleteTokenByUserId(INVALID_ID, TokenType.VERIFY);
+        assertEquals(3, JdbcTestUtils.countRowsInTable(jdbcTemplate, "verificationtokens"));
+    }
+
+    @Test
+    public void testGetToken() {
+        Optional<VerificationToken> verificationToken = verificationTokenDao.getToken(USER_VERIFY_TOKEN);
+        assertNotNull(verificationToken);
+        assertTrue(verificationToken.isPresent());
+        assertEquals(2, verificationToken.get().getId());
+        assertEquals(USER_VERIFY_TOKEN, verificationToken.get().getToken());
+        assertEquals(TokenType.VERIFY, verificationToken.get().getType());
+        assertEquals(verificationToken.get().getExpiryDate(), USER_VERIFY_TOKEN_EXPIRY_DATE);
+    }
 
     @Test
     public void testGetInvalidToken() {
--- paw-webapp/persistence/src/test/resources/verificationTokenDaoTest.sql
+++ paw-webapp/persistence/src/test/resources/verificationTokenDaoTest.sql
@@ -9,13 +9,13 @@ INSERT INTO users(id, email, password, name, surname, isband, isenabled, descrip
 VALUES (1, 'band@mail.com', '12345678', 'name', null, true, false, 'description', 1, false);
 
 INSERT INTO verificationtokens(tokenid, userid, token, expirydate, type)
-VALUES (1, 1, 'token-reset', '2022-07-05 14:23:30', 'reset');
+VALUES (1, 1, 'token-reset', '2022-07-05 14:23:30', 'RESET');
 
 INSERT INTO verificationtokens(tokenid, userid, token, expirydate, type)
-VALUES (2, 1, 'token-verify', '2022-07-05 14:23:30', 'verify');
+VALUES (2, 1, 'token-verify', '2022-07-05 14:23:30', 'VERIFY');
 
 INSERT INTO users(id, email, password, name, surname, isband, isenabled, description, locationid, available)
 VALUES (2, 'band2@mail.com', '12345678', 'name', null, true, false, 'description', 1, false);
 
 INSERT INTO verificationtokens(tokenid, userid, token, expirydate, type)
-VALUES (3, 2, 'token-reset-2', '2022-07-05 14:23:30', 'reset');
+VALUES (3, 2, 'token-reset-2', '2022-07-05 14:23:30', 'RESET');
