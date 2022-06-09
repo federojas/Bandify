1c3eb9a266cd2ef3f5a6db671c9828f8c2c3825f
Federico Rojas <frojas@itba.edu.ar>
2022-06-09 00:10:50 -0300

    Membership service done

--- paw-webapp/service/src/main/java/ar/edu/itba/paw/service/MembershipServiceImpl.java
+++ paw-webapp/service/src/main/java/ar/edu/itba/paw/service/MembershipServiceImpl.java
@@ -2,6 +2,12 @@ package ar.edu.itba.paw.service;
 
 import ar.edu.itba.paw.model.Membership;
 import ar.edu.itba.paw.model.User;
+import ar.edu.itba.paw.persistence.ApplicationDao;
+import ar.edu.itba.paw.persistence.MembershipDao;
+import com.sun.javaws.exceptions.InvalidArgumentException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,33 +15,34 @@ import java.util.List;
 @Service
 public class MembershipServiceImpl implements MembershipService {
 
+    @Autowired
+    private MembershipDao membershipDao;
+    @Autowired
+    private UserService userService;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MembershipServiceImpl.class);
+
     @Override
-    public List<Membership> getBandMemberships(User band, int page) {
-        return null;
+    public List<Membership> getUserMemberships(User user, int page) {
+        LOGGER.info("Getting specified band memberships");
+        return membershipDao.getUserMemberships(user, page);
     }
 
     @Override
-    public int getTotalBandMembershipsPages(User band) {
-        return 0;
+    public int getTotalUserMembershipsPages(User user) {
+        LOGGER.info("Getting total specified band memberships");
+        return membershipDao.getTotalUserMembershipsPages(user);
     }
 
     @Override
-    public void addMember(Membership.Builder builder) {
-
+    public Membership createMembership(Membership.Builder builder) {
+        LOGGER.info("Creating new membership");
+        return membershipDao.createMembership(builder);
     }
 
     @Override
-    public void deleteMembership(Membership membership) {
-
-    }
-
-    @Override
-    public List<Membership> getArtistMemberships(User artist, int page) {
-        return null;
-    }
-
-    @Override
-    public int getTotalArtistMembershipsPages(User band) {
-        return 0;
+    public void deleteMembership(long id) {
+        LOGGER.info("Deleting membership with id: {}", id);
+        membershipDao.deleteMembership(id);
     }
 }
