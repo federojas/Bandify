0b1408c8b9d5d4842650f8070a411c49712c8961
Federico Rojas <frojas@itba.edu.ar>
2022-07-27 14:16:43 -0300

    GET methods added to user controller

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
@@ -3,38 +3,45 @@ package ar.edu.itba.paw.webapp.controller;
 import ar.edu.itba.paw.model.FilterOptions;
 import ar.edu.itba.paw.model.User;
 import ar.edu.itba.paw.service.UserService;
+import ar.edu.itba.paw.webapp.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.jws.soap.SOAPBinding;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Filter;
+import java.util.stream.Collectors;
 
 @Path("users")
 @Component
 public class UserController {
+
     @Autowired
     private UserService us;
-//    @Context
-//    private UriInfo uriInfo;
+
+    @Context
+    private UriInfo uriInfo;
 
     @GET
     @Produces(value = { MediaType.APPLICATION_JSON, })
     public Response listUsers(@QueryParam("page") @DefaultValue("1") final int page) {
-        final List<User> allUsers = us.filter(new FilterOptions.FilterOptionsBuilder().withTitle("").withOrder("").withGenres(null).withLocations(null).withRoles(null).build(), page);
+        final List<UserDto> allUsers = us.filter(new FilterOptions.FilterOptionsBuilder().withTitle("").withOrder("").withGenres(null).withLocations(null).withRoles(null).build(), page)
+                .stream().map(u -> UserDto.fromUser(uriInfo, u)).collect(Collectors.toList());
         if(allUsers.isEmpty()) {
             return Response.noContent().build();
         }
-        //TODO links y user dto
-//        return Response.ok(new GenericEntity<List<User>>(allUsers) {}).build();
-        return Response.ok().build();
+
+
+        //TODO links MODULARIZAR PAGINATION LINKS
+        return Response.ok(new GenericEntity<List<UserDto>>(allUsers) {}).build();
     }
 
-    //register
+    //register TODO FORM DTO
 //    @POST
 //    @Produces(value = { MediaType.APPLICATION_JSON, })
 //    public Response createUser(final UserDTO userDto) {
@@ -44,19 +51,50 @@ public class UserController {
 //        return Response.created(uri).build();
 //    }
 //
-//    @GET
-//    @Path("/{id}")
-//    @Produces(value = { MediaType.APPLICATION_JSON, })
-//    public Response getById(@PathParam("id") final long id) {
-//        final Optional<User> user = us.getUserById(id);
-//        if (!user.isPresent()) {
-//            return Response.ok(new UserDTO(user)).build();
-//        } else {
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//    }
+    @GET
+    @Path("/{id}")
+    @Produces(value = { MediaType.APPLICATION_JSON, })
+    public Response getById(@PathParam("id") final long id) {
+        final Optional<User> user = us.getUserById(id);
+        //TODO links
+        if (user.isPresent())
+            return Response.ok(UserDto.fromUser(uriInfo, user.get())).build();
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
 
-    //NO HAY DELETE
+    @GET
+    @Path("/{id}/profile-image")
+    @Produces("application/vnd.campus.api.v1+json")
+    public Response getUserProfileImage(@PathParam("id") Long id) throws IOException {
+        //TODO links
+        return Response.ok(new ByteArrayInputStream(us.getProfilePicture(id))).build();
+    }
+
+    //TODO SEGUN LEI EL DEFAULT ES QUE LOS QUERY PARAM NO SON NUNCA OBLIGATORIOS, MANDAN NULL
+    @GET
+    @Path("/search")
+    @Produces(value = { MediaType.APPLICATION_JSON, })
+    public Response usersSearch(@QueryParam("page") @DefaultValue("1") final int page,
+                                @QueryParam("query") @DefaultValue("") final String query,
+                                @QueryParam("genre") final List<String> genres,
+                                @QueryParam("role") final List<String> roles,
+                                @QueryParam("location")  final List<String> locations) {
+        FilterOptions filter = new FilterOptions.FilterOptionsBuilder().
+                withGenres(genres)
+                .withRoles(roles)
+                .withLocations(locations)
+                .withTitle(query).build();
+        List<UserDto> users = us.filter(filter, page)
+                .stream().map(u -> UserDto.fromUser(uriInfo, u)).collect(Collectors.toList());;
+        if(users.isEmpty()) {
+            return Response.noContent().build();
+        }
+        //TODO links
+        return Response.ok(new GenericEntity<List<UserDto>>(users) {}).build();
+    }
+
+
+    //TODO NO HAY DELETE JERSEY MANDA 405 AUTOMATICAMENTE O HACERLO
 //    @DELETE
 //    @Path("/{id}")
 //    @Produces(value = { MediaType.APPLICATION_JSON, })
---
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/dto/UserDto.java
@@ -0,0 +1,179 @@
+package ar.edu.itba.paw.webapp.dto;
+
+import ar.edu.itba.paw.model.User;
+
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+import java.net.URI;
+
+public class UserDto {
+
+    private Long id;
+    private String email;
+    private String name;
+    private String surname;
+    private String description;
+    private boolean isBand;
+    private boolean isEnabled;
+    private boolean available;
+    private byte[] image;
+
+    private URI self;
+    private URI location;
+    private URI roles;
+    private URI genres;
+    private URI socialMedia;
+
+    public static UserDto fromUser(final UriInfo uriInfo, final User user) {
+        if(user == null)
+            return null;
+        UserDto dto = new UserDto();
+        dto.id = user.getId();
+        dto.name = user.getName();
+        dto.surname = user.getSurname();
+        dto.email = user.getEmail();
+        dto.description = user.getDescription();
+        dto.isBand = user.isBand();
+        dto.isEnabled = user.isEnabled();
+        dto.available = user.isAvailable();
+        dto.image = user.getProfileImage();
+
+        final UriBuilder userUriBuilder = uriInfo.getAbsolutePathBuilder()
+                .replacePath("users").path(String.valueOf(user.getId()));
+        dto.self = userUriBuilder.build();
+
+        final UriBuilder locationUriBuilder = uriInfo.getAbsolutePathBuilder()
+                .replacePath("users").path(String.valueOf(user.getId())).path("location");
+        dto.location = locationUriBuilder.build();
+
+        final UriBuilder roleUriBuilder = uriInfo.getAbsolutePathBuilder()
+                .replacePath("users").path(String.valueOf(user.getId())).path("roles");
+        dto.roles = roleUriBuilder.build();
+
+        final UriBuilder genreUriBuilder = uriInfo.getAbsolutePathBuilder()
+                .replacePath("users").path(String.valueOf(user.getId())).path("genres");
+        dto.genres = genreUriBuilder.build();
+
+        // TODO ACA IRIAN QUERY PARAM POR CADA SOCIAL MEDIA TYPE?
+        final UriBuilder socialUriBuilder = uriInfo.getAbsolutePathBuilder()
+                .replacePath("users").path(String.valueOf(user.getId())).path("social-media");
+        dto.socialMedia = socialUriBuilder.build();
+
+        //TODO LINKS/URIS VER OPCIONES CHEQUEAR Y REVISAR
+
+        return dto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isBand() {
+        return isBand;
+    }
+
+    public void setBand(boolean band) {
+        isBand = band;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        isEnabled = enabled;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public URI getSelf() {
+        return self;
+    }
+
+    public void setSelf(URI self) {
+        this.self = self;
+    }
+
+    public URI getLocation() {
+        return location;
+    }
+
+    public void setLocation(URI location) {
+        this.location = location;
+    }
+
+    public URI getRoles() {
+        return roles;
+    }
+
+    public void setRoles(URI roles) {
+        this.roles = roles;
+    }
+
+    public URI getGenres() {
+        return genres;
+    }
+
+    public void setGenres(URI genres) {
+        this.genres = genres;
+    }
+
+    public URI getSocialMedia() {
+        return socialMedia;
+    }
+
+    public void setSocialMedia(URI socialMedia) {
+        this.socialMedia = socialMedia;
+    }
+}
