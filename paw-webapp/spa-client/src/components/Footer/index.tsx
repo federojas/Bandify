b0b986392252a58985430ef6836f47cab8da5d1d
Leonardo D'Agostino <ldagostino@itba.edu.ar>
2022-12-07 12:40:22 -0300

    fix: Footer

--- paw-webapp/spa-client/src/components/Footer/index.tsx
+++ paw-webapp/spa-client/src/components/Footer/index.tsx
@@ -1,20 +1,26 @@
 import { FooterCopyright, Lang, PageFooter } from "./styles";
+import "../../styles/footer.css";
 
 export default function Footer() {
   return (
-    <PageFooter>
-      <FooterCopyright>
-        Copyright
-        <div>
-            <b>Contact us</b>
-            <a href="mailto: ${mail}">a</a>
+    <footer className="page-footer">
+      <div className="footer-copyright">
+        <div className="flex flex-row justify-between px-10">
+          © 2022 Copyright: Bandify
+          <div>
+            <b>Contact us: </b>
+            <a href="mailto: bandifypaw@gmail.com">bandifypaw@gmail</a>
+          </div>
+          <div>
+            <a className="languages-buttons right" href="?lang=es">
+              ES
+            </a>
+            <a className="languages-buttons right" href="?lang=en">
+              EN
+            </a>
+          </div>
         </div>
-        <div>
-            <Lang href="">ES</Lang>
-            <Lang href="">EN</Lang>
-        </div>
-      </FooterCopyright>
- 
-    </PageFooter>
+      </div>
+    </footer>
   );
 }
