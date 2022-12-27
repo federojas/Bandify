21e7643596f56cf9a2143829d9abe1061edc7058
Leonardo D'Agostino <ldagostino@itba.edu.ar>
2022-12-27 19:14:39 -0300

    feat: Starting of react-hook-form on Artist Register

--- paw-webapp/spa-client/src/components/RegisterArtistForm/index.tsx
+++ paw-webapp/spa-client/src/components/RegisterArtistForm/index.tsx
@@ -3,19 +3,71 @@ import "../../styles/alerts.css";
 import "../../js/register.js";
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { useForm } from "react-hook-form";
+
+interface FormData {
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+  name: string;
+  surname: string;
+}
 
 const RegisterArtistForm = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<FormData>();
 
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
 
-    return (
-        <div className="register-content">
-      <form
-        action="/registerBand"
-        method="post"
-        acceptCharset="utf-8"
-        className="box"
-      >
+  const registerArtistOptions = {
+    email: {
+      required: "Email is required",
+      maxLength: {
+        value: 254,
+        message: "Email cannot be larger than 254 characters",
+      },
+      pattern: /^\S+@\S+$/i,
+    },
+    password: {
+      required: "Password is required",
+      minLength: {
+        value: 8,
+        message: "Password must be at least 8 characters",
+      },
+      maxLength: {
+        value: 25,
+        message: "Password cannot be larger than 25 characters",
+      },
+    },
+    passwordConfirmation: {
+      required: "Password confirmation is required",
+    },
+    name: {
+      required: "Name is required",
+      maxLength: {
+        value: 50,
+        message: "Name cannot be larger than 50 characters",
+      },
+    },
+    surname: {
+      required: "Surname is required",
+      maxLength: {
+        value: 50,
+        message: "Surname cannot be larger than 50 characters",
+      },
+    },
+  };
+
+  return (
+    <div className="register-content">
+      <form onSubmit={handleSubmit(onSubmit)} className="box">
         <div>
           <label className="form-label" htmlFor="email">
             {t("Register.email")}
@@ -23,16 +75,17 @@ const RegisterArtistForm = () => {
           {/* Message for register.form.emailplaceholder */}
           {/* TODO placeholder={/* emailplaceholder } */}
           <input
-            type="text"
-            maxLength={254}
-            id="bandEmaill"
+            type="email"
+            maxLength={255}
+            // id="bandEmaill"
             className="form-input"
-            name="email"
+            // name="email"
+            {...register("email", registerArtistOptions.email)}
             placeholder={t("Register.email_placeholder")}
           />
           {/* form errors for email */}
-          <p className="error" id="wrongMail" style={{ display: "none" }}>
-            {/* Message for Email.userBandForm.email */}
+          <p className="error" id="wrongMail">
+            {errors.email && <span>{errors.email.message}</span>}
           </p>
         </div>
 
@@ -48,11 +101,14 @@ const RegisterArtistForm = () => {
             maxLength={25}
             placeholder={t("Register.password_placeholder")}
             className="form-input"
-            name="password"
-            onKeyUp={() => checkPasswordBand()}
+            // name="password"
+            {...register("password", registerArtistOptions.password)}
+            // onKeyUp={() => checkPasswordBand()}
+            // TODO: Check both passwords match
           />
           {/* form errors for password */}
-          <p className="error" id="emptyPass" style={{ display: "none" }}>
+          <p className="error" id="emptyPass">
+            {errors.password && <span>{errors.password.message}</span>}
             {/* Message for register.form.emptyPassword with arguments 8, 25 */}
           </p>
         </div>
@@ -69,22 +125,28 @@ const RegisterArtistForm = () => {
             maxLength={25}
             placeholder={t("Register.password_placeholder")}
             className="form-input"
-            name="passwordConfirmation"
-            onKeyUp={() => checkPasswordArtist()}
+            // name="passwordConfirmation"
+            {...register("passwordConfirmation", registerArtistOptions.passwordConfirmation)}
+            // onKeyUp={() => checkPasswordArtist()}
+            // TODO: Check both passwords match
           />
+          <p className="error" id="emptyPass">
+            {errors.passwordConfirmation && <span>{errors.passwordConfirmation.message}</span>}
+            {/* Message for register.form.emptyPassword with arguments 8, 25 */}
+          </p>
           {/* form errors for passwordConfirmation */}
           <span
             id="match_message"
             className="success"
-            style={{ display: "none" }}
           >
+              {/* TODO: Message for matching passwords  */}
             {/* Message for register.form.passwordmatch */}
           </span>
           <span
             id="nonmatch_message"
             className="error"
-            style={{ display: "none" }}
           >
+            {/* {errors.pass} */}
             {/* Message for register.form.passwordnomatch */}
           </span>
         </div>
@@ -101,11 +163,13 @@ const RegisterArtistForm = () => {
             placeholder={t("Register.artist_name")}
             className="form-input"
             id="artistName"
-            name="name"
+            // name="name"
+            {...register("name", registerArtistOptions.name)}
           />
           {/* form errors for name */}
           <p className="error" id="wrongName" style={{ display: "none" }}>
             {/* Message for register.form.invalidName */}
+            {errors.name && <span>{errors.name.message}</span>}
           </p>
         </div>
 
@@ -121,11 +185,13 @@ const RegisterArtistForm = () => {
             placeholder={t("Register.artist_surname")}
             className="form-input"
             id="artistSurname"
-            name="name"
+            // name="name"
+            {...register("surname", registerArtistOptions.surname)}
           />
           {/* form errors for name */}
           <p className="error" id="wrongName" style={{ display: "none" }}>
             {/* Message for register.form.invalidName */}
+            {errors.surname && <span>{errors.surname.message}</span>}
           </p>
         </div>
 
@@ -137,7 +203,7 @@ const RegisterArtistForm = () => {
         </div>
       </form>
     </div>
-    )
-}
+  );
+};
 
 export default RegisterArtistForm;
