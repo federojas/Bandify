77c3e10083dcc69629e3aebd0355af21324a9a3a
Leonardo D'Agostino <ldagostino@itba.edu.ar>
2022-12-28 10:33:55 -0300

    feat: New register page (Hub for Band or Artist Registers)

--- paw-webapp/spa-client/src/pages/Register/index.tsx
+++ paw-webapp/spa-client/src/pages/Register/index.tsx
@@ -1,72 +1,167 @@
-import * as React from "react";
+import ArtistRegisterAvatar from "../../images/artistRegisterAvatar.jpg";
+import BandRegisterAvatar from "../../images/bandRegisterAvatar.jpg";
 
-import { BandifyLogoImg } from "../../components/NavBar/styles";
-import BandifyLogo from "../../images/logo.png";
-
+import {
+  Avatar,
+  Box,
+  chakra,
+  Flex,
+  Icon,
+  SimpleGrid,
+  useColorModeValue,
+  Button,
+  Link
+} from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
-
 import "../../styles/forms.css";
 import "../../styles/register.css";
 import "../../js/alerts.js";
 
-import RegisterBandForm from "../../components/RegisterBandForm";
+const registers = [
+  {
+    content:
+      "By being an artist, you can apply to band's auditions and get a chance to be part of a band.",
+    avatar: ArtistRegisterAvatar,
+    url: "/registerArtist",
+    title: "Register as an Artist",
+    urlTitle: "I'm an artist",
+  },
+  {
+    content: "By being a band, you can create auditions and find new members.",
+    avatar: BandRegisterAvatar,
+    url: "/registerBand",
+    title: "Register as a Band",
+    urlTitle: "I'm a band",
+  },
+];
+
+const backgrounds = [
+  `url("data:image/svg+xml, %3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'560\' height=\'185\' viewBox=\'0 0 560 185\' fill=\'none\'%3E%3Cellipse cx=\'102.633\' cy=\'61.0737\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23ED64A6\' /%3E%3Cellipse cx=\'399.573\' cy=\'123.926\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23F56565\' /%3E%3Cellipse cx=\'366.192\' cy=\'73.2292\' rx=\'193.808\' ry=\'73.2292\' fill=\'%2338B2AC\' /%3E%3Cellipse cx=\'222.705\' cy=\'110.585\' rx=\'193.808\' ry=\'73.2292\' fill=\'%23ED8936\' /%3E%3C/svg%3E")`,
+  `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='457.367' cy='123.926' rx='102.633' ry='61.0737' transform='rotate(-180 457.367 123.926)' fill='%23ED8936'/%3E%3Cellipse cx='160.427' cy='61.0737' rx='102.633' ry='61.0737' transform='rotate(-180 160.427 61.0737)' fill='%2348BB78'/%3E%3Cellipse cx='193.808' cy='111.771' rx='193.808' ry='73.2292' transform='rotate(-180 193.808 111.771)' fill='%230BC5EA'/%3E%3Cellipse cx='337.295' cy='74.415' rx='193.808' ry='73.2292' transform='rotate(-180 337.295 74.415)' fill='%23ED64A6'/%3E%3C/svg%3E")`,
+  `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='102.633' cy='61.0737' rx='102.633' ry='61.0737' fill='%23ED8936'/%3E%3Cellipse cx='399.573' cy='123.926' rx='102.633' ry='61.0737' fill='%2348BB78'/%3E%3Cellipse cx='366.192' cy='73.2292' rx='193.808' ry='73.2292' fill='%230BC5EA'/%3E%3Cellipse cx='222.705' cy='110.585' rx='193.808' ry='73.2292' fill='%23ED64A6'/%3E%3C/svg%3E")`,
+  `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='457.367' cy='123.926' rx='102.633' ry='61.0737' transform='rotate(-180 457.367 123.926)' fill='%23ECC94B'/%3E%3Cellipse cx='160.427' cy='61.0737' rx='102.633' ry='61.0737' transform='rotate(-180 160.427 61.0737)' fill='%239F7AEA'/%3E%3Cellipse cx='193.808' cy='111.771' rx='193.808' ry='73.2292' transform='rotate(-180 193.808 111.771)' fill='%234299E1'/%3E%3Cellipse cx='337.295' cy='74.415' rx='193.808' ry='73.2292' transform='rotate(-180 337.295 74.415)' fill='%2348BB78'/%3E%3C/svg%3E")`,
+];
+
+interface RegisterCardProps {
+  content: string;
+  avatar: string;
+  index: number;
+  title: string;
+  url: string;
+  urlTitle: string;
+}
+
+function RegisterCard(props: RegisterCardProps) {
+  const { content, avatar, index, urlTitle, url, title } = props;
+  return (
+    <Flex
+      boxShadow={"lg"}
+      maxW={"640px"}
+      direction={{ base: "column-reverse", md: "row" }}
+      width={"full"}
+      rounded={"xl"}
+      p={10}
+      justifyContent={"space-between"}
+      position={"relative"}
+      bg={useColorModeValue("white", "gray.800")}
+      _before={{
+        content: '""',
+        position: "absolute",
+        zIndex: "-1",
+        height: "full",
+        maxW: "640px",
+        width: "full",
+        filter: "blur(40px)",
+        transform: "scale(0.98)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        top: 0,
+        left: 0,
+        backgroundImage: backgrounds[index % 4],
+      }}
+    >
+      <Flex
+        direction={"column"}
+        textAlign={"left"}
+        justifyContent={"space-between"}
+      >
+        <chakra.h2 fontWeight={"bold"} fontSize={20} pb={4}>
+          {title}
+        </chakra.h2>
+        <chakra.p
+          // fontFamily={"Inter"}
+          fontWeight={"medium"}
+          fontSize={"15px"}
+          pb={4}
+        >
+          {content}
+        </chakra.p>
+        <Link href={url}><Button colorScheme={"blue"}>{urlTitle}</Button></Link>
+        
+      </Flex>
+      <Avatar
+        src={avatar}
+        height={"80px"}
+        width={"80px"}
+        alignSelf={"center"}
+        m={{ base: "0 0 35px 0", md: "0 0 0 50px" }}
+      />
+    </Flex>
+  );
+}
 
 const RegisterContent = () => {
   const { t } = useTranslation();
 
   return (
-    // <div>
-    //   {t("Register.header")}
-    //   <a href="/registerBand">{t("Register.band")}</a>
-    //   <a href="/registerArtist">{t("Register.artist")}</a>
-    // </div>
-
-    <main>
-      <div className="register-content flex flex-col">
-        <div className="card-content">
-          <div className="header">
-            {t("Register.header")}
-            <div className="forms-buttons">
-              <a href="/registerArtist">
-
-                  {t("Register.artist")}
-              </a>
-              <a href="/registerBand">
-                {t("Register.band")}
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-    //       {(
-    //         <>
-    //           <div id="artist-form" style={{ display: 'block' }}>
-    //             {/* TODO Include the registerArtistForm component here */}
-    //             {/* <RegisterArtistForm artist={1} /> */}
-    //           </div>
-    //           <div id="band-form" style={{ display: 'none' }}>
-    //             {/* TODO Include the registerBandForm component here */}
-    //             <RegisterBandForm />
-    //           </div>
-    //         </>
-    //       ) : (
-    //         <>
-    //           <div id="artist-form" style={{ display: 'none' }}>
-    //             {/* TODO Include the registerArtistForm component here */}
-    //             {/* <RegisterArtistForm artist={1} /> */}
-    //           </div>
-    //           <div id="band-form" style={{ display: 'block' }}>
-    //             {/* TODO  Include the registerBandForm component here */}
-    //             <RegisterBandForm/>
-    //           </div>
-    //         </>
-    //       )}
-    //     </div>
-    //   </div>
-    //   <div id="snackbar">Snackbar.message</div>
-    // </main>
+    <Flex
+      textAlign={"center"}
+      pt={10}
+      justifyContent={"center"}
+      direction={"column"}
+      width={"full"}
+    >
+      <Box width={{ base: "full", sm: "lg", lg: "xl" }} margin={"auto"}>
+        <chakra.h1
+          py={5}
+          fontSize={48}
+          // fontFamily={"Work Sans"}
+          fontWeight={"bold"}
+          color={useColorModeValue("gray.700", "gray.50")}
+        >
+          Register your band or artist for free!
+        </chakra.h1>
+        <chakra.h2
+          margin={"auto"}
+          width={"70%"}
+          // fontFamily={"Inter"}
+          fontWeight={"medium"}
+          color={useColorModeValue("gray.500", "gray.400")}
+        >
+          Get to know new artists and bands, and get discovered!
+        </chakra.h2>
+      </Box>
+      <SimpleGrid
+        columns={{ base: 1, xl: 2 }}
+        spacing={"20"}
+        mt={16}
+        mx={"auto"}
+      >
+        {registers.map((cardInfo, index) => (
+          <RegisterCard {...cardInfo} index={index} />
+        ))}
+      </SimpleGrid>
+      <Box>
+        <Icon viewBox="0 0 40 35" mt={14} boxSize={10} color={"purple.400"}>
+          <path
+            fill={"currentColor"}
+            d="M10.7964 5.04553e-07C8.66112 -0.000123335 6.57374 0.632971 4.79827 1.81922C3.0228 3.00547 1.63898 4.69158 0.82182 6.66433C0.00466116 8.63708 -0.209132 10.8079 0.207477 12.9021C0.624087 14.9964 1.65239 16.9201 3.16233 18.4299L19.1153 34.3828C19.2395 34.5074 19.3871 34.6062 19.5496 34.6736C19.7121 34.741 19.8863 34.7757 20.0622 34.7757C20.2381 34.7757 20.4123 34.741 20.5748 34.6736C20.7373 34.6062 20.8848 34.5074 21.0091 34.3828L36.962 18.4272C38.9319 16.3917 40.0228 13.6636 39.9996 10.8311C39.9764 7.99858 38.8409 5.28867 36.838 3.28573C34.835 1.28279 32.1251 0.147283 29.2926 0.124081C26.4601 0.100879 23.732 1.19184 21.6965 3.1617L20.0622 4.79337L18.4305 3.1617C17.4276 2.15892 16.237 1.36356 14.9267 0.821064C13.6163 0.278568 12.2119 -0.000433066 10.7937 5.04553e-07H10.7964Z"
+          />
+        </Icon>
+      </Box>
+    </Flex>
   );
-};
+}
+
 
 export default RegisterContent;
