fd54a804bbbe3e3526a36404e8e1671dff6375d5
robrodriguez <robroriguez@itba.edu.ar>
2023-02-04 18:34:05 -0300

    chore: separate component into new file

--- paw-webapp/spa-client/src/pages/Audition/index.tsx
+++ paw-webapp/spa-client/src/pages/Audition/index.tsx
@@ -46,83 +46,8 @@ import { useUserService } from "../../contexts/UserService";
 import { useAuditionService } from "../../contexts/AuditionService";
 import AuthContext from "../../contexts/AuthContext";
 import swal from 'sweetalert';
-import { useForm } from "react-hook-form";
-import { applyAuditionOptions } from "./validations";
+import ApplyButton from "./ApplyAudition";
 
-interface FormData {
-  description: string;
-}
-
-const ApplyButton = ({auditionId} : {auditionId: number})  => {
-  const { t } = useTranslation();
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const auditionService = useAuditionService();
-  const initialRef = React.useRef(null)
-  const finalRef = React.useRef(null)
-  const navigate = useNavigate();
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>();
-
-
-  const onSubmit = (data: FormData) => {
-      serviceCall(auditionService.apply( auditionId, data.description),
-      navigate
-      ).then((response) => {//todo: distintos response messages segun el error
-        if(response.hasFailed()) {
-          swal({
-            title: t("Audition.applyError"),
-            icon: "error",
-          })
-        } else {
-          swal({
-            title: t("Audition.applySuccess"),
-            text: t("Audition.applySuccessText"),
-            icon: "success",
-          })
-        }
-      })
-  }
-
-  return (
-    <>
-      <Modal
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={isOpen}
-        onClose={onClose}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>{t("Audition.Modal.title")}</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <FormControl>
-                <FormLabel>{t("Audition.Modal.message")}</FormLabel>
-                <Textarea 
-                  // ref={initialRef}
-                  placeholder={t("Audition.Modal.placeHolder")}
-                  maxLength={300}
-                  {...register("description", applyAuditionOptions.message)}
-                  />
-              </FormControl>
-              <ModalFooter>
-                <Button colorScheme='blue' mr={3} type="submit">
-                  {t("Audition.Modal.apply")}
-                </Button>
-                <Button onClick={onClose}>{t("Audition.Modal.cancel")}</Button>
-              </ModalFooter>
-            </form>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    </>
-  )
-}
 
 const AuditionActions = (props: { auditionId: number, isOwner: boolean, currentUser: User | undefined }) => {
   const isBand = props.currentUser?.band;
