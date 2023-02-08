603fcbce3bc1118beb080da152d29d9fed6e8c3e
Leonardo <ldagostino@itba.edu.ar>
2023-02-07 22:15:20 -0300

    BREAKING: npm install !

--- paw-webapp/spa-client/src/components/ToggleColorMode/index.tsx
+++ paw-webapp/spa-client/src/components/ToggleColorMode/index.tsx
@@ -1,12 +1,26 @@
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { Button, useColorMode } from "@chakra-ui/react";
+import useSound from "use-sound";
 
 const ToggleColorMode = () => {
-    const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [play] = useSound("/lightswitch.mp3", {
+    volume: 0.05,
+    sprite: {
+      on: [0, 300],
+      off: [500, 300],
+    },
+  });
 
-    return (
-<Button onClick={toggleColorMode} mx={4}>{colorMode === 'dark' ? <SunIcon color="orange.200" /> : <MoonIcon color="blue.700"/>}</Button>
-    );
+
+  const handleClick = () => {
+    toggleColorMode();
+    colorMode === "dark" ? play({ id: "on" }) : play({ id: "off" });
+  };
+
+  return (
+    <Button onClick={handleClick} mx={4}>{colorMode === 'dark' ? <SunIcon color="orange.200" /> : <MoonIcon color="blue.700" />}</Button>
+  );
 }
 
 export default ToggleColorMode;
