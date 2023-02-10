73d385d1aefe12863c8bc8bc2723b558fa0cb215
Leonardo <ldagostino@itba.edu.ar>
2023-02-09 22:01:33 -0300

    fix: kick member / leave band

--- paw-webapp/spa-client/src/pages/User/MembershipItem.tsx
+++ paw-webapp/spa-client/src/pages/User/MembershipItem.tsx
@@ -9,46 +9,46 @@ import { User } from "../../models";
 import { serviceCall } from "../../services/ServiceManager";
 
 
-function HandleAccept (membershipId:number){
-  console.log("entre");
-  const membershipService = useMembershipService();
-  const toast = useToast();
-  const { t } = useTranslation();
-  const navigate = useNavigate();
-
-  serviceCall(
-    membershipService.kickMember(membershipId),
-    navigate
-  ).then((response) => {
-    if (!response.hasFailed()) {
-      toast({
-        title: t("Profile.kickModal.success"),
-        description: t("Profile.kickModal.successDescription"),
-        status: "success",
-        duration: 9000,
-        isClosable: true,
-      });
-    } else {
-      toast({
-        title: t("Profile.kickModal.error"),
-        description: t("Profile.kickModal.errorDescription"),
-        status: "error",
-        duration: 9000,
-        isClosable: true,
-      });
-    }
-  });
-
-}
-
-function LeaveBand({membershipId}:{membershipId:number}) {
+function LeaveBand({ membershipId }: { membershipId: number }) {
   const { t } = useTranslation();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  
+  const membershipService = useMembershipService();
+  const toast = useToast();
+  const navigate = useNavigate();
+
+
+  const handleAccept = (membershipId: number) => {
+
+    serviceCall(
+      membershipService.kickMember(membershipId),
+      navigate
+    ).then((response) => {
+      if (!response.hasFailed()) {
+        toast({
+          title: t("Profile.kickModal.success"),
+          description: t("Profile.kickModal.successDescription"),
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: t("Profile.kickModal.error"),
+          description: t("Profile.kickModal.errorDescription"),
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+      onClose()
+    });
+
+  }
+
   return (
     <>
-    <Button colorScheme='red' onClick={onOpen}>{t("Profile.leaveBand")}</Button>
-    <Modal isOpen={isOpen} onClose={onClose}>
+      <Button colorScheme='red' onClick={onOpen}>{t("Profile.leaveBand")}</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>{t("Profile.leaveModal.title")}</ModalHeader>
@@ -58,7 +58,7 @@ function LeaveBand({membershipId}:{membershipId:number}) {
           </ModalBody>
 
           <ModalFooter>
-            <Button leftIcon={<TiTick />} colorScheme='blue' mr={3} onClick={()=>HandleAccept(membershipId)}>
+            <Button leftIcon={<TiTick />} colorScheme='blue' mr={3} onClick={() => handleAccept(membershipId)}>
               {t("Profile.leaveModal.confirm")}
             </Button>
             <Button leftIcon={<TiCancel />} colorScheme='red' onClick={onClose} >{t("Profile.leaveModal.cancel")}</Button>
@@ -69,14 +69,47 @@ function LeaveBand({membershipId}:{membershipId:number}) {
   )
 }
 
-function KickFromBand({membershipId}:{membershipId:number}) {
+function KickFromBand({ membershipId }: { membershipId: number }) {
   const { t } = useTranslation();
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const membershipService = useMembershipService();
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  const handleAccept = (membershipId: number) => {
+
+    serviceCall(
+      membershipService.kickMember(membershipId),
+      navigate
+    ).then((response) => {
+      if (!response.hasFailed()) {
+        toast({
+          title: t("Profile.kickModal.success"),
+          description: t("Profile.kickModal.successDescription"),
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: t("Profile.kickModal.error"),
+          description: t("Profile.kickModal.errorDescription"),
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+      onClose()
+
+    });
+
+  }
+
 
   return (
     <>
-    <Button onClick={onOpen} colorScheme='red'>{t("Profile.kickFromBand")}</Button>
-    <Modal isOpen={isOpen} onClose={onClose}>
+      <Button onClick={onOpen} colorScheme='red'>{t("Profile.kickFromBand")}</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>{t("Profile.kickModal.title")}</ModalHeader>
@@ -86,7 +119,7 @@ function KickFromBand({membershipId}:{membershipId:number}) {
           </ModalBody>
 
           <ModalFooter>
-            <Button leftIcon={<TiTick />} colorScheme='blue' mr={3} onClick={()=>HandleAccept(membershipId)}>
+            <Button leftIcon={<TiTick />} colorScheme='blue' mr={3} onClick={() => handleAccept(membershipId)}>
               {t("Profile.kickModal.confirm")}
             </Button>
             <Button leftIcon={<TiCancel />} colorScheme='red' onClick={onClose} >{t("Profile.kickModal.cancel")}</Button>
@@ -97,37 +130,37 @@ function KickFromBand({membershipId}:{membershipId:number}) {
   )
 }
 
-const MembershipItem = ({ contraUser, description, roles, isOwner, membershipId  }: { contraUser: User, description: string, roles: string[], isOwner: boolean, membershipId: number}) => {
-  const { t } = useTranslation(); 
+const MembershipItem = ({ contraUser, description, roles, isOwner, membershipId }: { contraUser: User, description: string, roles: string[], isOwner: boolean, membershipId: number }) => {
+  const { t } = useTranslation();
   const navigate = useNavigate();
   return (
     <Box borderWidth='1px' borderRadius='lg' p="4" w={'full'}>
       <Flex direction={'column'} justify="space-between">
         <VStack justify='space-between'>
-        <HStack onClick={() => {
-          navigate('/users/' + contraUser.id)
-        }}
-          cursor={'pointer'}
-        >
-          <Avatar src={contraUser.profileImage} //TODO: revisar ALT?
-            _dark={{
-              backgroundColor: "white",
-            }} />
-          <Box ml='3'>
-            <Text fontWeight='bold'>
-              {contraUser.name}
-              {
-                contraUser.surname && ` ${contraUser.surname}`
-              }
-            </Text>
-          </Box>
-        </HStack>
+          <HStack onClick={() => {
+            navigate('/users/' + contraUser.id)
+          }}
+            cursor={'pointer'}
+          >
+            <Avatar src={contraUser.profileImage} //TODO: revisar ALT?
+              _dark={{
+                backgroundColor: "white",
+              }} />
+            <Box ml='3'>
+              <Text fontWeight='bold'>
+                {contraUser.name}
+                {
+                  contraUser.surname && ` ${contraUser.surname}`
+                }
+              </Text>
+            </Box>
+          </HStack>
           <Box>
-            {isOwner ? 
-            (contraUser.band ?
-              <LeaveBand membershipId={membershipId as any} />:
-              <KickFromBand membershipId={membershipId} />)
-             : <></>}
+            {isOwner ?
+              (contraUser.band ?
+                <LeaveBand membershipId={membershipId as any} /> :
+                <KickFromBand membershipId={membershipId} />)
+              : <></>}
           </Box>
         </VStack>
         <Flex direction={'column'} justify={'space-between'} alignItems={'center'}>
