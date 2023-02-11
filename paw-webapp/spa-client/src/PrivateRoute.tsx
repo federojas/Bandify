e1679e5160d61b87eff5758a511e3cd442e5cedd
Leonardo <ldagostino@itba.edu.ar>
2023-02-10 23:51:58 -0300

    login redirect fix

--- paw-webapp/spa-client/src/PrivateRoute.tsx
+++ paw-webapp/spa-client/src/PrivateRoute.tsx
@@ -1,24 +1,25 @@
-import { Navigate } from 'react-router-dom'
+import { Navigate, useLocation } from 'react-router-dom'
 import AuthContext from "./contexts/AuthContext";
 import { useContext } from "react";
 // TODO: en caso de que el usuario no estaba logueado, estaria bueno redireccionarlo
 // TODO: despues del login al path que habia querido ir antes.
 interface Props {
-    component: React.ComponentType
-    path?: string
-    roles: Array<string>
+  component: React.ComponentType
+  path?: string
+  roles: Array<string>
 }
 
 export const PrivateRoute: React.FC<Props> = ({ component: RouteComponent, roles }) => {
+  const { isAuthenticated, role } = useContext(AuthContext);
+  const location = useLocation();
 
-const { isAuthenticated, role } = useContext(AuthContext);
-if (isAuthenticated && roles.includes(role as string)) {
+  if (isAuthenticated && roles.includes(role as string)) {
     return <RouteComponent />
-}
+  }
 
-if (isAuthenticated && !roles.includes(role as string)) {
+  if (isAuthenticated && !roles.includes(role as string)) {
     return <Navigate to="/error?code=403" />
-}
+  }
 
-return <Navigate to="/login" />
+  return <Navigate to='/login' state={{ prev: location.pathname }} />
 }
