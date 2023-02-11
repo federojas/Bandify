6b91a8a03ee6e0a460bb66438c563b2173dc9dae
Leonardo <ldagostino@itba.edu.ar>
2023-02-10 21:22:12 -0300

    fix: fixed membership item edit

--- paw-webapp/spa-client/src/pages/Profile/MembershipItemEdit.tsx
+++ paw-webapp/spa-client/src/pages/Profile/MembershipItemEdit.tsx
@@ -3,51 +3,54 @@ import { useNavigate } from "react-router-dom";
 import RoleTag from "../../components/Tags/RoleTag";
 import { User } from "../../models";
 import EditMembershipButton from "./EditMembershipButton";
-import DeleteMembershipButton from"./DeleteMembershipButton"
+import DeleteMembershipButton from "./DeleteMembershipButton"
 
 const MembershipItemEdit = ({ contraUser, description, roles, membershipId, refresh }: { contraUser: User, description: string, roles: string[], membershipId: number, refresh: () => void }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <Box borderWidth='1px' borderRadius='lg' p="4" w={'full'}>
-            <Flex direction={'column'} justify="space-between">
-                <VStack justify='space-between'>
-                    <HStack onClick={() => {
-                        navigate('/users/' + contraUser.id)
-                    }}
-                            cursor={'pointer'}
-                    >
-                        <Avatar src={contraUser.profileImage}
-                                _dark={{
-                                    backgroundColor: "white",
-                                }} />
-                        <Box ml='3'>
-                            <Text fontWeight='bold'>
-                                {contraUser.name}
-                                {
-                                    contraUser.surname && ` ${contraUser.surname}`
-                                }
-                            </Text>
-                        </Box>
-                    </HStack>
-                    <Box>
-                        <EditMembershipButton membershipId={membershipId} rolesParam={roles} descriptionParam={description} refresh={refresh} />
-                        <DeleteMembershipButton membershipId={membershipId} refresh={refresh} />
-                    </Box>
-                </VStack>
-                <Flex direction={'column'} justify={'space-between'} alignItems={'center'} mt={5}>
-                    <HStack>
-                        {roles.map((role) => {
-                            return (
-                                <RoleTag key={role} role={role} size="md" />
-                            )
-                        })}
-                    </HStack>
-                    <Text as='i'>{description}</Text>
-                </Flex>
-            </Flex>
-        </Box>
-    )
+  return (
+    <Box borderWidth='1px' borderRadius='lg' p="4" w={'full'}>
+      <Flex direction={'row'} justify="space-between">
+        <Flex direction={'column'} flex={3} justify="start">
+          <HStack onClick={() => {
+            navigate('/users/' + contraUser.id)
+          }}
+            cursor={'pointer'}
+          >
+            <Avatar src={contraUser.profileImage}
+              _dark={{
+                backgroundColor: "white",
+              }} />
+            <Box ml='3'>
+              <Text fontWeight='bold'>
+                {contraUser.name}
+                {
+                  contraUser.surname && ` ${contraUser.surname}`
+                }
+              </Text>
+            </Box>
+          </HStack>
+
+          <Flex direction={'column'} justify={'start'} alignItems={'start'} mt={5}>
+            <HStack>
+              {roles.map((role) => {
+                return (
+                  <RoleTag key={role} role={role} size="md" />
+                )
+              })}
+            </HStack>
+            <Text as='i'>{description}</Text>
+          </Flex>
+        </Flex>
+
+        <Flex direction={'column'} flex={1} justify="space-around" align={'space-between'}>
+          {/*<LeaveBand membershipId={membershipId as any} refresh={refresh} /> */}
+          <EditMembershipButton membershipId={membershipId} rolesParam={roles} descriptionParam={description} refresh={refresh} />
+          <DeleteMembershipButton membershipId={membershipId} refresh={refresh} />
+        </Flex>
+      </Flex>
+    </Box>
+  )
 }
 
 export default MembershipItemEdit
