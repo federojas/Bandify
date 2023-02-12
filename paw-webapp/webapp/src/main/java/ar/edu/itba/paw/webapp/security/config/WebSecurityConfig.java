0b71a90016af9d1415969925b5d18ee15b85aa1c
Federico Rojas <frojas@itba.edu.ar>
2023-02-12 15:38:26 -0300

    l

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/security/config/WebSecurityConfig.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/security/config/WebSecurityConfig.java
@@ -21,7 +21,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
